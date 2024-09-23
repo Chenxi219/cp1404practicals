5541d08686d8296b6830097f392c9eba20fae9bd
wang chenxi <chenxi.wang@my.jcu.edu.au>
2024-09-23 18:28:52 +0800

    Add prac1

---
+++ prac_01/menus.py
@@ -0,0 +1,13 @@
+name = input("Enter name: ")
+print("(H)ellow\n(G)oodbye\n(Q)uit")
+choice = input(">>>").upper()
+while choice != "Q":
+    if choice == "H":
+        print(f"Hellow {name}")
+    elif choice == "G":
+        print(f"Goodbye {name}")
+    else:
+        print("Invalid choice")
+    print("(H)ellow\n(G)oodbye\n(Q)uit")
+    choice = input(">>>").upper()
+print("Finished")
