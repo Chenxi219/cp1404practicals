f47ed6ea6b9802daed0f7584a4003b53cfb3b3ec
wang chenxi <chenxi.wang@my.jcu.edu.au>
2024-09-24 10:04:43 +0800

    Refactor password check program to use functions

---
+++ prac_02/passwords_stars.py
@@ -0,0 +1,17 @@
+def get_password():
+    global min_length
+    min_length = 5
+
+
+get_password()
+
+def main():
+    password = input("Enter a password: ")
+    determine_password(password)
+    print('*' * len(password))
+
+def determine_password(password):
+    while len(password) <= min_length:
+        print("Password must more than five")
+        password = input("Enter a password: ")
+    return password
