8caec6c63bec9e825a503088fe29035775d641d4
wang chenxi <chenxi.wang@my.jcu.edu.au>
2024-09-24 10:51:32 +0800

    change broken score to score.py add random score

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,14 +1,25 @@
+import random
 MINIMUM_SCORE = 0
 PASS_THRESHOLD = 50
 MAXIMUM_SCORE = 100
 PASSABLE_SCORE = 90
 
-score = float(input("Enter score: "))
-if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
-    print("Invalid score")
-elif score < PASS_THRESHOLD:
-    print("Bad")
-elif score < PASSABLE_SCORE:
-    print("Passable")
-else:
-    print("Excellent")
+def main():
+    score = float(input("Enter score: "))
+    feedback = print_feedback(score)
+    print(feedback)
+    random_score = random.randint(0,100)
+    print(random_score)
+    random_feedback = print_feedback(random_score)
+    print(random_feedback)
+
+def print_feedback(score):
+    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
+        return "Invalid score"
+    elif score < PASS_THRESHOLD:
+        return "Bad"
+    elif score < PASSABLE_SCORE:
+        return "Passable"
+    else:
+        return "Excellent"
+main()
