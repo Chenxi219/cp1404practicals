e9e93e40df3a3eb43d304c9f339057062188df35
wang chenxi <chenxi.wang@my.jcu.edu.au>
2024-11-06 19:35:37 +0800

    finish prac 07 project exercise

---
+++ prac_07/project.py
@@ -0,0 +1,21 @@
+"""
+Project
+Estimate: 10 minutes
+Actual:   15 minutes
+"""
+class Project:
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        self.name = name
+        self.start_date = start_date
+        self.priority = int(priority)
+        self.cost_estimate = float(cost_estimate)
+        self.completion_percentage = int(completion_percentage)
+
+    def __str__(self):
+        return f'{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, completion: {self.completion_percentage}%'
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+    def is_complete(self):
+        return self.completion_percentage == 100
