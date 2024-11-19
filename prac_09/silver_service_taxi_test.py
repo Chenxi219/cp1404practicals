7629ee950740360774da2e0b00e5e64843634c0e
wang chenxi <chenxi.wang@my.jcu.edu.au>
2024-11-19 10:47:29 +0800

    add prac 09 SilverServiceTaxi exercise

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,8 @@
+from silver_service_taxi import SilverServiceTaxi
+
+def main():
+    my_silver_service_taxi = SilverServiceTaxi("SilverServiceTaxi", 200, 2)
+    my_silver_service_taxi.drive(18)
+    print(my_silver_service_taxi)
+
+main()
